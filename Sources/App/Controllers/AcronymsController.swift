ed8954f2878c2bb7c3b324ccc2c5dc00d583acd6
risingone <anuragdhingra101@gmail.com>
2018-04-24 15:07:26 +0530

    Adding all the CRUD api's

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -5,6 +5,9 @@ struct AcronymController: RouteCollection {
         let acronymsRoute = router.grouped("api","acronyms")
         acronymsRoute.get(use: getAllHandler)
         acronymsRoute.post(use: createHandler)
+        acronymsRoute.get(Acronym.parameter, use: getHandler)
+        acronymsRoute.delete(Acronym.parameter, use: deleteHandler)
+        acronymsRoute.put(Acronym.parameter, use: updateHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -15,4 +18,24 @@ struct AcronymController: RouteCollection {
         let acronym = try req.content.decode(Acronym.self)
         return acronym.save(on: req)
     }
+    
+    func getHandler(_ req: Request) throws -> Future<Acronym> {
+        return try req.parameters.next(Acronym.self)
+    }
+    
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(Acronym.self).flatMap(to: HTTPStatus.self) { acronym in
+            return acronym.delete(on: req).transform(to: .noContent)
+        }
+    }
+    
+    func updateHandler(_ req: Request) throws ->  Future<Acronym> {
+        return try flatMap(to: Acronym.self, req.parameter(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        }
+    }
  }
+
+extension Acronym: Parameter {}
